dd84a19a65aeacd0d3e3b88a011b3bee6144bfbd
Kripta-Studios <kriptastudios@tutamail.com>
2021-03-20 11:21:36 +0100

    kajdadj

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main(void) 
 {
-    printf("In develop right now,\n main files are hidden for bugs reasons");
+    printf("In develop right now.\nMain files are hidden for bugs reasons");
     return 0;
 } 
